1797a10a6a4dc97e1b297699c089118ca96eadfe
Daniel Aguirre <daniel_agui98@outlook.com>
2022-09-27 19:45:08 -0300

    Europa

--- appviajandoando/urls.py
+++ appviajandoando/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from appviajandoando.views import principal
+from appviajandoando.views import europa, principal, login_user, registrate
+
+#import para Statics Files
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+#import para cerrar sesión
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path ('', principal, name='principal'),
+    path ('login_user/', login_user, name="login_user"),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path ('registrate/', registrate, name="registrate"),
+    path('europa/', europa, name='europa')
 
 ]
 
--- appviajandoando/views.py
+++ appviajandoando/views.py
@@ -1,9 +1,56 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 from appviajandoando.forms import *
 from appviajandoando.models import *
 
+# import para registrar un Usuario
+from .forms import UserRegisterForm
+from .models import *
+#imports para login
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from datetime import datetime
+
 # Create your views here.
 
 def principal(request):
     return render(request, 'index.html')
+
+def login_user(request):
+    if request.method=="POST":
+        form = AuthenticationForm(request, data=request.POST )
+        if form.is_valid():
+            usu=request.POST["username"]
+            clave=request.POST["password"]
+
+            usuario=authenticate(username=usu, password=clave)
+            if usuario is not None:
+                login(request, usuario)
+                return render(request, 'login_user.html', {'mensaje':f"Bienvenido {usuario}"})
+            else:
+                return render(request, 'login_user.html', {"form":form, 'mensaje':'Usuario o contraseña incorrectos'})
+        else:
+            return render(request, 'login_user.html', {"form":form, 'mensaje':'Usuario o contraseña incorrectos'})
+    else:
+        form=AuthenticationForm()
+        return render(request, 'login_user.html', {'form':form})
+
+
+def registrate(request):
+    if request.method=="POST":
+        form= UserRegisterForm(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data["username"]
+            #podriamos fijarnos que no exista un user en la bd con ese nombre
+
+            form.save()
+            return render(request, 'login_user.html', {'mensaje':f"Usuario {username} creado!"})
+    else:
+        form=UserRegisterForm()
+    return render(request, 'registrate.html', {'form':form})
+
+
+def europa(request):
+    return render(request , 'europa.html')
